f9ff61a21556230db142e2b9a5c2bcece1c3094a
Erika Pauwels <erika.pauwels@tenforce.com>
2016-01-16 20:21:28 +0100

    Add logging
    - Add log helper method
    - Set default log level to INFO
    - Log SPARQL queries at INFO level
    - Add debug logs to helper methods

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM erikap/ruby-sinatra:ruby-2.1-latest
 MAINTAINER Erika Pauwels <erika.pauwels@gmail.com>
 
 ENV APP_ENTRYPOINT web.rb
+ENV LOG_LEVEL info
 
 ADD . /usr/src/app
 
--- web.rb
+++ web.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'logger'
 require 'sparql/client'
 require 'json'
 require 'rdf/vocab'
@@ -7,8 +8,17 @@ require 'bson'
 configure do
   set :graph, ENV['MU_APPLICATION_GRAPH']
   set :sparql_client, SPARQL::Client.new('http://database:8890/sparql')
-end
 
+  ###
+  # Logging
+  ###
+  log_dir = '/logs'
+  Dir.mkdir(log_dir) unless Dir.exist?(log_dir)
+  # Keep 10 log files of 100 MB in size
+  log = Logger.new("#{log_dir}/#{settings.environment}.log", 10, 100*1024*1024)
+  log.level = Kernel.const_get("Logger::#{ENV['LOG_LEVEL'].upcase}")
+  set :log, log
+end
 
 ###
 # Vocabularies
@@ -27,31 +37,40 @@ helpers do
     BSON::ObjectId.new.to_s
   end
 
+  def log
+    settings.log
+  end
+
   def session_id_header(request)
+    log.debug "Get HTTP_MU_SESSION_ID request header from #{request.env.inspect}"
     request.env['HTTP_MU_SESSION_ID']
   end
 
   def rewrite_url_header(request)
+    log.debug "Get HTTP_X_REWRITE_URL request header from #{request.env.inspect}"
     request.env['HTTP_X_REWRITE_URL']
   end
 
   def error(title, status = 400)
+    log.error "HTTP status #{status}: #{title}"
     halt status, { errors: [{ title: title }] }.to_json
   end
 
   def validate_json_api_content_type(request)
-    error('Content-Type must be application/vnd.api+json') if not request.env['CONTENT_TYPE'] == 'application/vnd.api+json'
+    error("Content-Type must be application/vnd.api+json instead of #{request_env['CONTENT_TYPE']}.") if not request.env['CONTENT_TYPE'] == 'application/vnd.api+json'
   end
 
   def validate_resource_type(expected_type, data)
-    error("Incorrect type. Type must be #{data['type']}", 409) if data['type'] != expected_type
+    error("Incorrect type. Type must be #{expected_type}, instead of #{data['type']}.", 409) if data['type'] != expected_type
   end
 
   def query(query)
+    log.info "Executing query: #{query}"
     settings.sparql_client.query query
   end
 
   def update(query)
+    log.info "Executing query: #{query}"
     settings.sparql_client.update query
   end
 
@@ -63,7 +82,7 @@ helpers do
     query += " WHERE {"
     query += "   <#{subject}> <#{RDF::Vocab::DC.modified}> ?modified ."
     query += " }"
-    settings.sparql_client.update(query)
+    update(query)
 
     query =  " INSERT DATA {"
     query += "   GRAPH <#{settings.graph}> {"
