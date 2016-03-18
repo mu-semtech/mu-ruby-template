b7b11e77c7f892a02ebd2146bc6bf82037f4bfef
nielsv <niels.vandekeybus@tenforce.com>
2016-03-18 15:31:51 +0100

    test helpers

--- Gemfile
+++ Gemfile
@@ -3,10 +3,13 @@ source 'https://rubygems.org'
 gem 'sinatra', '1.4.6'
 
 gem 'bson', '4.0.0'
-gem 'json', '1.8.3'
 gem 'rdf', '1.99.1'
 gem 'rdf-vocab', '~> 0.8.7'
 gem 'sparql-client', '1.99.0', require: 'sparql/client'
+
+group :test, :development do
+	gem 'rspec', '~> 3.4'
+end
 
 Dir.glob(File.join(File.dirname(__FILE__), 'ext', '**', "Gemfile")) do |gemfile|
     eval(IO.read(gemfile), binding)
---
+++ sinatra_template/helpers.rb
@@ -0,0 +1,62 @@
+module SinatraTemplate
+  module Helpers
+    def generate_uuid
+      BSON::ObjectId.new.to_s
+    end
+  
+    def log
+      settings.log
+    end
+  
+    def session_id_header(request)
+      log.debug "Get HTTP_MU_SESSION_ID request header from #{request.env.inspect}"
+      request.env['HTTP_MU_SESSION_ID']
+    end
+  
+    def rewrite_url_header(request)
+      log.debug "Get HTTP_X_REWRITE_URL request header from #{request.env.inspect}"
+      request.env['HTTP_X_REWRITE_URL']
+    end
+  
+    def error(title, status = 400)
+      log.error "HTTP status #{status}: #{title}"
+      halt status, { errors: [{ title: title }] }.to_json
+    end
+  
+    def validate_json_api_content_type(request)
+      error("Content-Type must be application/vnd.api+json instead of #{request_env['CONTENT_TYPE']}.") if not request.env['CONTENT_TYPE'] =~ /^application\/vnd\.api\+json/
+    end
+  
+    def validate_resource_type(expected_type, data)
+      error("Incorrect type. Type must be #{expected_type}, instead of #{data['type']}.", 409) if data['type'] != expected_type
+    end
+  
+    def query(query)
+      log.info "Executing query: #{query}"
+      settings.sparql_client.query query
+    end
+  
+    def update(query)
+      log.info "Executing query: #{query}"
+      settings.sparql_client.update query
+    end
+  
+    def update_modified(subject, modified = DateTime.now.xmlschema)
+      query =  " WITH <#{settings.graph}> "
+      query += " DELETE {"
+      query += "   <#{subject}> <#{RDF::Vocab::DC.modified}> ?modified ."
+      query += " }"
+      query += " WHERE {"
+      query += "   <#{subject}> <#{RDF::Vocab::DC.modified}> ?modified ."
+      query += " }"
+      update(query)
+  
+      query =  " INSERT DATA {"
+      query += "   GRAPH <#{settings.graph}> {"
+      query += "     <#{subject}> <#{RDF::Vocab::DC.modified}> \"#{modified}\"^^xsd:dateTime ."
+      query += "   }"
+      query += " }"
+      update(query)
+    end
+  end
+end
