a869c4b76d5bdb62438c0d7ddd8ce04e81dba50c
Niels Vandekeybus <progster@gmail.com>
2019-09-03 11:02:49 +0200

    allow specification of JRUBY_OPTIONS to pass extra options to jruby
    example usage: docker run -e JRUBY_OPTIONS="-J-Xmx32g" ...

--- startup.sh
+++ startup.sh
@@ -2,13 +2,13 @@
 if [ "$RACK_ENV" == "production" ];
 then
     bundle install --without development test
-    bundle exec ruby --server $MAIN_APP_FILE -p 80
+    bundle exec ruby $JRUBY_OPTIONS --server $MAIN_APP_FILE -p 80
 else
     bundle install
     if [ "$RACK_ENV" == "test" ];
     then
         bundle exec rspec
     else
-        bundle exec ruby --dev $MAIN_APP_FILE -p 80 -o '0.0.0.0'
+        bundle exec ruby $JRUBY_OPTIONS --dev $MAIN_APP_FILE -p 80 -o '0.0.0.0'
     fi
 fi
